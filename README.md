a3aa905d647c1fceea2e9697d3e0c1f68ec2fc50
Glyph <glyph@twistedmatrix.com>
2014-08-21 02:38:59 -0700

    Begin.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Automat #
+
+## Self-service finite-state machines for the programmer on the go. ##
+
+```python
+
+from automat import MethodicalMachine
+
+class Automat(object):
+    machine = MethodicalMachine()
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+Setup file for mimic
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Automat',
+    version='0.1.0',
+    description="""
+    Self-service finite-state machines for the programmer on the go.
+    """,
+    packages=find_packages(exclude=[]),
+    package_dir={'automat': 'automat'},
+    install_requires=[
+        "characteristic",
+        "six",
+    ],
+    include_package_data=True,
+    license="MIT",
+)
