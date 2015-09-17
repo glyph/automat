1ce1a43672b37feb7145f07bf943a961373ff381
Glyph <glyph@twistedmatrix.com>
2015-09-17 04:41:15 -0700

    a couple more assertions, docs; detect the state

--- automat/_test/test_methodical.py
+++ automat/_test/test_methodical.py
@@ -270,13 +270,17 @@ class MethodicalTests(TestCase):
 
     def test_restoreState(self):
         """
-        
+        L{MethodicalMachine.unserializer} decorates a function that becomes a
+        machine-state unserializer; its return value is mapped to the
+        C{serialized} parameter to C{state}, and the L{MethodicalMachine}
+        associated with that instance's state is updated to that state.
         """
 
         class Mechanism(object):
             m = MethodicalMachine()
             def __init__(self):
                 self.value = 1
+                self.ranOutput = False
             @m.state(serialized="first-state", initial=True)
             def first(self):
                 "First state."
@@ -289,7 +293,15 @@ class MethodicalTests(TestCase):
             @m.output()
             def output(self):
                 self.value = 2
-            first.upon(input, second, [output])
+                self.ranOutput = True
+                return 1
+            @m.output()
+            def output2(self):
+                return 2
+            first.upon(input, second, [output],
+                       collector=lambda x: list(x)[0])
+            second.upon(input, second, [output2],
+                        collector=lambda x: list(x)[0])
             @m.serializer()
             def save(self, state):
                 return {
@@ -312,6 +324,8 @@ class MethodicalTests(TestCase):
         m1.input()
         blob = m1.save()
         m2 = Mechanism.fromBlob(blob)
+        self.assertEqual(m2.ranOutput, False)
+        self.assertEqual(m2.input(), 2)
         self.assertEqual(
             m2.save(),
             {
