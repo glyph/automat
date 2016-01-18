9f19b20ac1860e345a560ab86d0c7110779167ba
Mark Williams <markrwilliams@gmail.com>
2016-01-18 15:14:48 -0800

    add a test that ensures addTransition requires an iterable for outputs

--- automat/_test/test_core.py
+++ automat/_test/test_core.py
@@ -33,6 +33,22 @@ class CoreTests(TestCase):
                          ("ending", ["end"]))
         self.assertEqual(a.states(), set(["beginning", "ending"]))
 
+
+    def test_oneTransition_nonIterableOutputs(self):
+        """
+        L{Automaton.addTransition} raises a TypeError when given outputs
+        that aren't iterable and doesn't add any transitions.
+        """
+        a = Automaton()
+        nonIterableOutputs = 1
+        self.assertRaises(
+            TypeError,
+            a.addTransition,
+            "fromState", "viaSymbol", "toState", nonIterableOutputs)
+        self.assertFalse(a.inputAlphabet())
+        self.assertFalse(a.outputAlphabet())
+        self.assertFalse(a.states())
+        self.assertFalse(a.allTransitions())
+
 # FIXME: addTransition for transition that's been added before
-# FIXME: addTransition with a non-iterable for outputs
 # FIXME: public API for determining initial states
