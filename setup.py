eef0cf792334cb39fc5bf8966913c62e9789c590
John W. O'Brien <john@saltant.com>
2017-02-17 19:46:56 -0500

    Conform author metadata to `distutils.core` API
    Update the `setup` keyword args for author contact to align with the
    [`distutils.core` API](https://docs.python.org/3.6/distutils/apiref.html).

--- setup.py
+++ setup.py
@@ -39,8 +39,8 @@ setup(
             "automat-visualize = automat._visualize:tool"
         ],
     },
-    author_name='Glyph',
-    author_mail='glyph@twistedmatrix.com',
+    author='Glyph',
+    author_email='glyph@twistedmatrix.com',
     include_package_data=True,
     license="MIT",
     keywords='fsm finite state machine automata',
