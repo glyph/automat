301be8dfbfd340099866175f9334e0315e8fe749
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-20 16:52:50 -0700

    Braino: next => itemgetter(0)
    ... avoids
    
       TypeError: 'list' object is not an iterator

--- docs/examples/lightswitch.py
+++ docs/examples/lightswitch.py
@@ -1,7 +1,11 @@
+from operator import itemgetter
+
 from automat import MethodicalMachine
+
 
 class LightSwitch(object):
     machine = MethodicalMachine()
+
     @machine.state(serialized="on")
     def on_state(self):
         "the switch is on"
@@ -13,27 +17,37 @@ class LightSwitch(object):
         "flip the switch"
     on_state.upon(flip, enter=off_state, outputs=[])
     off_state.upon(flip, enter=on_state, outputs=[])
+
     @machine.input()
     def query_power(self):
         "return True if powered, False otherwise"
     @machine.output()
     def _is_powered(self):
         return True
+
     @machine.output()
     def _not_powered(self):
         return False
     on_state.upon(query_power, enter=on_state, outputs=[_is_powered],
-                  collector=next)
+                  collector=itemgetter(0))
     off_state.upon(query_power, enter=off_state, outputs=[_not_powered],
-                   collector=next)
+                   collector=itemgetter(0))
+
     @machine.serializer()
     def save(self, state):
         return {"is-it-on": state}
+
     @machine.unserializer()
     def _restore(self, blob):
         return blob["is-it-on"]
+
     @classmethod
     def from_blob(cls, blob):
         self = cls()
         self._restore(blob)
         return self
+
+
+if __name__ == "__main__":
+    l = LightSwitch()
+    print(l.query_power())
