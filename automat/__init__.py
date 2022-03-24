da680e58d713b1ae5873e01340928055202d3cad
Glyph <glyph@twistedmatrix.com>
2022-03-24 14:24:58 -0700

    expose it

--- automat/__init__.py
+++ automat/__init__.py
@@ -1,8 +1,10 @@
 # -*- test-case-name: automat -*-
 from ._methodical import MethodicalMachine
 from ._core import NoTransition
+from ._typical import ClusterStateDecorator as TypicalMachine
 
 __all__ = [
     'MethodicalMachine',
+    'TypicalMachine',
     'NoTransition',
 ]
