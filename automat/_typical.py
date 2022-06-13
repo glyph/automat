94fbe17ac6e41cceb1cb2782cf33fb7fa3b57163
Glyph <glyph@twistedmatrix.com>
2022-06-12 21:31:30 -0700

    bug workarounds, rip out examples

--- automat/_typical.py
+++ automat/_typical.py
@@ -50,17 +50,13 @@ OutputCallable = TypeVar("OutputCallable", bound=Callable[..., Any])
 
 
 class ProtocolAtRuntime(Protocol[InputsProto]):
-    __name__: str
-
+    # __name__: str # https://github.com/python/mypy/issues/12976
     def __call__(self) -> InputsProto:
         ...
 
 
-class HasName(Protocol):
-    __name__: str
-
-
-InputMethod = TypeVar("InputMethod", bound=HasName)
+def _name(x: ProtocolAtRuntime[T]) -> str:
+    return x.__name__  # type:ignore[attr-defined]
 
 
 class CouldNotFindAutoParam(RuntimeError):
@@ -205,6 +201,8 @@ def _bindableTransitionMethod(
                 raise RuntimeError("unhandled state transition")
             else:
                 realMethod = getattr(stateObject, outputMethodName)
+                if realMethod.__automat_handler__[-1]:
+                    a = (self, *a)
                 result = realMethod(*a, **kw)
         finally:
             _updateState(oldState, self, a, kw, stateFactories, inputMethod)
@@ -338,7 +336,7 @@ class TypicalBuilder(Generic[InputsProto, StateCore, P]):
                         continue
                     inputMethod: Callable[..., object]
                     enter: Optional[Callable[[], Type[object]]]
-                    [inputMethod, enter] = ah
+                    [inputMethod, enter, shouldPassSelf] = ah
                     newStateType = stateClass if enter is None else enter()
                     inputName = inputMethod.__name__
                     if inputName not in possibleInputs:
@@ -375,7 +373,7 @@ class TypicalBuilder(Generic[InputsProto, StateCore, P]):
             self._stateClasses[0],
             automaton,
             type(
-                f"Machine<{self._stateProtocol.__name__}>",
+                f"Machine<{_name(self._stateProtocol)}>",
                 tuple([_TypicalInstance]),
                 {
                     **ns,
@@ -416,7 +414,25 @@ class TypicalBuilder(Generic[InputsProto, StateCore, P]):
         """
 
         def decorator(c: OutputCallable) -> OutputCallable:
-            c.__automat_handler__ = [input, enter]  # type: ignore
+            c.__automat_handler__ = [input, enter, False]  # type: ignore
+            return c
+
+        return decorator
+
+    def handle2(
+        self,
+        input: Callable[Concatenate[SelfA, ThisInputArgs], R],
+        enter: Optional[Callable[[], Type[object]]] = None,
+    ) -> Callable[
+        [Callable[Concatenate[SelfB, InputsProto, ThisInputArgs], R]],
+        Callable[Concatenate[SelfB, InputsProto, ThisInputArgs], R],
+    ]:
+        """
+        Define an input handler.
+        """
+
+        def decorator(c: OutputCallable) -> OutputCallable:
+            c.__automat_handler__ = [input, enter, True]  # type: ignore
             return c
 
         return decorator
@@ -477,229 +493,3 @@ class TypicalBuilder(Generic[InputsProto, StateCore, P]):
             return f
 
         return decorator
-
-
-if __name__ == "__main__":
-
-    class CoffeeMachine(Protocol):
-        def put_in_beans(self, beans: str) -> None:
-            "put in some beans"
-
-        def brew_button(self) -> None:
-            "press the brew button"
-
-    @dataclass
-    class BrewerStateCore(object):
-        heat: int = 0
-
-    coffee = TypicalBuilder(CoffeeMachine, BrewerStateCore)
-
-    @coffee.state()
-    class NoBeanHaver(object):
-        @coffee.handle(CoffeeMachine.brew_button)
-        def no_beans(self) -> None:
-            print("no beans, not heating")
-
-        @coffee.handle(CoffeeMachine.put_in_beans, enter=lambda: BeanHaver)
-        def add_beans(self, beans) -> None:
-            print("put in some beans", repr(beans))
-
-    @coffee.state(persist=False)
-    @dataclass
-    class BeanHaver:
-        core: BrewerStateCore
-        beans: str
-
-        @coffee.handle(CoffeeMachine.brew_button, enter=lambda: NoBeanHaver)
-        def heat_the_heating_element(self) -> None:
-            self.core.heat += 1
-            print("yay brewing:", repr(self.beans))
-
-        @coffee.handle(CoffeeMachine.put_in_beans, enter=lambda: BeanHaver)
-        def too_many_beans(self, beans: object) -> None:
-            print("beans overflowing:", repr(beans), self.beans)
-
-    """
-    Need a better example that has a start, handshake, and established state
-    class, and the established state class can have a reference to an instance
-    of the handshake state class, and can thereby access its state.
-    """
-
-    CoffeeStateMachine = coffee.buildClass()
-    print("Created:", CoffeeStateMachine)
-    x: CoffeeMachine = CoffeeStateMachine(3)
-    print(isinstance(x, CoffeeStateMachine))
-    x.brew_button()
-    x.brew_button()
-    x.put_in_beans("old beans")
-    x.put_in_beans("oops too many beans")
-    x.brew_button()
-    x.brew_button()
-    x.put_in_beans("new beans")
-    x.brew_button()
-
-    @dataclass
-    class ControlPlane(object):
-        _cb: Callable[[Callable[[Turnstile], None]], None]
-        _pending_operation: str | None = None
-        _money_counter: int = 0
-
-        def __post_init__(self) -> None:
-            # Hmm. Don't love this pattern for handing portions of the state
-            # core back to the caller...
-            @self._cb
-            def complete_operation(t: Turnstile) -> None:
-                o, self._pending_operation = self._pending_operation, None
-                match o:
-                    case "lock":
-                        t.arm_lock_engaged()
-                    case "unlock":
-                        t.arm_lock_disengaged()
-                        t.arm_rotated()
-                    case None:
-                        t.token_inserted()
-
-        def lock(self) -> None:
-            assert self._pending_operation is None
-            self._pending_operation = "lock"
-
-        def unlock(self) -> None:
-            assert self._pending_operation is None
-            self._pending_operation = "unlock"
-
-        def reset(self) -> None:
-            self._pending_operation = None
-
-    class Turnstile(Protocol):
-        def kick(self) -> None:
-            ...
-
-        def token_inserted(self) -> None:
-            ...
-
-        def arm_rotated(self) -> None:
-            ...
-
-        def arm_lock_engaged(self) -> None:
-            ...
-
-        def arm_lock_disengaged(self) -> None:
-            ...
-
-        def repair(self) -> None:
-            ...
-
-    turn = TypicalBuilder(Turnstile, ControlPlane)
-
-    # You can use .implement to have wrapper implementations that apply in all
-    # states.  Note that these methods will execute even in error states, so if
-    # you need to bail out in error conditions make sure to call something on
-    # your public-protocol first argument.
-
-    @turn.implement(Turnstile.kick)
-    def kick(t: Turnstile, p: ControlPlane) -> None:
-        print("BANG")
-
-    # You can also define *internal* protocols that your state classes can use.
-    # Mypy will not make these methods visible to your callers, although they
-    # are present at runtime.
-    class InternalTurnstile(Protocol):
-        def _add_token(self) -> int:
-            pass
-
-        def _enough_tokens(self) -> None:
-            ...
-
-    # If you ask for an internal interface, it will be passed along with the
-    # public interface and state core.  Internal interfaces like this can be
-    # used for "private" inputs; i.e. inputs to the state machine which should
-    # only be generated when certain conditions are met, such as a counter
-    # exceeding a threshold as shown here.
-    @turn.implement(Turnstile.token_inserted, InternalTurnstile)
-    def count_money(t: Turnstile, p: ControlPlane, private: InternalTurnstile) -> None:
-        print("**plink**")
-        if private._add_token() == 3:
-            private._enough_tokens()
-
-    @turn.state(persist=False)
-    @dataclass
-    class Unpaid(object):
-        "Locked, not paid"
-        plane: ControlPlane
-        # persist=False above means this gets reset every time we exit this
-        # state.
-        money: int = 0
-
-        @turn.handle(InternalTurnstile._add_token)
-        def pay(self) -> int:
-            self.money += 1
-            return self.money
-
-        @turn.handle(InternalTurnstile._enough_tokens, enter=lambda: Unlocking)
-        def paid(self) -> None:
-            print("requesting unlock")
-            self.plane.unlock()
-
-    @turn.state()
-    class Unlocking(object):
-        "Paid, not unlocked yet."
-
-        @turn.handle(Turnstile.arm_lock_disengaged, enter=lambda: Paid)
-        def ready(self) -> None:
-            print("unlocked, waiting for customer to walk through")
-
-    @turn.state()
-    @dataclass
-    class Paid(object):
-        "Paid and unlocked."
-        plane: ControlPlane
-
-        @turn.handle(Turnstile.arm_rotated, enter=lambda: Locking)
-        def relock(self) -> None:
-            print("customer walked through, locking")
-            self.plane.lock()
-
-    @turn.state()
-    class Locking(object):
-        "Fare consumed, not yet locked."
-
-        @turn.handle(Turnstile.arm_lock_engaged, enter=lambda: Unpaid)
-        def engaged(self) -> None:
-            print("finished locking")
-
-    @turn.state(error=True)
-    @dataclass
-    class Broken(object):
-        plane: ControlPlane
-
-        @turn.handle(Turnstile.repair, enter=lambda: Unpaid)
-        def repair(self) -> None:
-            self.plane.reset()
-
-    Turner = turn.buildClass()
-    loops: List[Callable[[Turnstile], None]] = []
-    t = Turner(loops.append)
-    [loop] = loops
-    print()
-    print("turnstile example:")
-    t.kick()
-    for _ in range(10):
-        loop(t)
-
-    print("haywire messages from microcontroller")
-    import traceback
-
-    try:
-        t.arm_rotated()
-    except:
-        traceback.print_exc()
-        print("handled")
-    try:
-        t.arm_rotated()
-    except:
-        traceback.print_exc()
-        print("still broken, fixing")
-        t.repair()
-    # fixed now
-    for _ in range(10):
-        loop(t)
