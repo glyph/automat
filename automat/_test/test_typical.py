86410ea264a6b8dba23ddf4d2409708b39b1e5ea
Glyph <glyph@twistedmatrix.com>
2022-06-16 17:57:05 -0700

    some more coverage

--- automat/_test/test_typical.py
+++ automat/_test/test_typical.py
@@ -11,6 +11,18 @@ class OneInput(Protocol):
     def one(self) -> int:
         "It's the input"
 
+    def depcheck(self, count: str) -> str:
+        """
+        Check populating of dependencies.
+
+        Parameter of conflicting type should not be used.
+        """
+
+    def valcheck(self) -> int:
+        """
+        check on the value
+        """
+
 class StateCore(object):
     "It's a state core"
     count: int = 0
@@ -29,6 +41,23 @@ class TheState(object):
         self.core.count += 1
         return self.core.count
 
+    @builder.handle(OneInput.depcheck, enter=lambda: CoreDataRequirer)
+    def from_core(self, count: str):
+        return count
+
+
+@builder.state()
+@dataclass
+class CoreDataRequirer(object):
+    """
+    I require data supplied by the state core.
+    """
+    count: int
+
+    @builder.handle(OneInput.valcheck)
+    def get(self) -> int:
+        return self.count
+
 C = builder.buildClass()
 
 class TypicalTests(TestCase):
@@ -45,3 +74,15 @@ class TypicalTests(TestCase):
 
         self.assertEqual(i.one(), 1)
         self.assertEqual(i.one(), 2)
+
+    def test_required_from_core(self) -> None:
+        """
+        Parameters required by state class which match a type/name pair exactly
+        from the state core will be populated from there (state input
+        parameters will be ignored if they are the wrong type).
+        """
+        i = C()
+        i.one()
+        i.one()
+        i.depcheck("ignore")
+        self.assertEqual(i.valcheck(), 2)
